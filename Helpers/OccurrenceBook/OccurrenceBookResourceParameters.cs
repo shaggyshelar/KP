f64323567bf2be6157424ef5c9af636315db9a42
Piyush Ostwal <Piyush.Ostwal>
2017-05-02 16:20:40 +0530

    Implementation Filter sorting for Occurrence Book and Employee entities

--- Helpers/OccurrenceBook/OccurrenceBookResourceParameters.cs
+++ Helpers/OccurrenceBook/OccurrenceBookResourceParameters.cs
@@ -1,9 +1,21 @@
+using System.Collections.Generic;
 using ESPL.KP.Helpers.Core;
 
 namespace ESPL.KP.Helpers.OccurrenceBook
 {
-    public class OccurrenceBookResourceParameters:BaseResourceParameters
+    public class OccurrenceBookResourceParameters : BaseResourceParameters
     {
-         public string OrderBy { get; set; } = "OBTime";
+        public OccurrenceBookResourceParameters()
+        {
+            this.OBDates = new List<string>();
+            this.StatusIDs = new List<string>();
+            this.AreaIDs = new List<string>();
+            this.DepartmentIDs = new List<string>();
+        }
+        public string OrderBy { get; set; } = "OBTime";
+        public List<string> OBDates { get; set; }
+        public List<string> StatusIDs { get; set; }
+        public List<string> AreaIDs { get; set; }
+        public List<string> DepartmentIDs { get; set; }
     }
 }
