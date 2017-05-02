0ceb2088e5bac6b5b4f31185edacafa20c9f52e5
Piyush Ostwal <Piyush.Ostwal>
2017-05-02 12:50:48 +0530

    Added New Entity "OccurrencStatusHistory"

---
+++ Entities/OccurrenceStatusHistory.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ESPL.KP.Entities
+{
+    public class OccurrenceStatusHistory : BaseEntity 
+    {
+        [Key]
+        public Guid OccurrenceStatusHistoryID { get; set; }
+
+        [ForeignKey ("OBID")]
+        public virtual MstOccurrenceBook MstOccurrenceBook { get; set; }
+        public Guid OBID { get; set; }
+
+        [ForeignKey("StatusID")]
+        public MstStatus MstStatus { get; set; }
+        public Guid StatusID { get; set; }
+
+        [MaxLength(500)]
+        public string Commnets { get; set; }
+
+        
+    }
+}
