9625d317526685de150c7b5a045a3907a0ad77e7
ManishPatil159 <manish.patil159@gmail.com>
2017-05-18 18:31:48 +0530

    Implemented the changes to remove maxPaging constraints for Designation and Departments.

--- Helpers/Department/DepartmentsResourceParameters.cs
+++ Helpers/Department/DepartmentsResourceParameters.cs
@@ -5,5 +5,17 @@ namespace ESPL.KP.Helpers.Department
     public class DepartmentsResourceParameters : BaseResourceParameters
     {
         public string OrderBy { get; set; } = "DepartmentName";
+        private int _pageSize = 0;
+        public override int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = value;
+            }
+        }
     }
 }
