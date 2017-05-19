f9175e80d88f2870561a3cf639b626b752e77702
ManishPatil159 <manish.patil159@gmail.com>
2017-05-19 15:02:45 +0530

    Reverting changes for Designation and Department

--- Helpers/Department/DepartmentsResourceParameters.cs
+++ Helpers/Department/DepartmentsResourceParameters.cs
@@ -4,18 +4,6 @@ namespace ESPL.KP.Helpers.Department
 {
     public class DepartmentsResourceParameters : BaseResourceParameters
     {
-        public string OrderBy { get; set; } = "DepartmentName";
-        private int _pageSize = 0;
-        public override int PageSize
-        {
-            get
-            {
-                return _pageSize;
-            }
-            set
-            {
-                _pageSize = value;
-            }
-        }
+        public string OrderBy { get; set; } = "DepartmentName";        
     }
 }
--- Helpers/Designation/DesignationsResourceParameters.cs
+++ Helpers/Designation/DesignationsResourceParameters.cs
@@ -4,18 +4,6 @@ namespace ESPL.KP.Helpers.Designation
 {
     public class DesignationsResourceParameters : BaseResourceParameters
     {
-        public string OrderBy { get; set; } = "DesignationName";
-        private int _pageSize = 0;
-        public override int PageSize
-        {
-            get
-            {
-                return _pageSize;
-            }
-            set
-            {
-                _pageSize = value;
-            }
-        }
+        public string OrderBy { get; set; } = "DesignationName";        
     }
 }
