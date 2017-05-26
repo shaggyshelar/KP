c1da9c63158e3ce1268318cb95dd08a716e8d262
ManishPatil159 <manish.patil159@gmail.com>
2017-05-26 12:09:21 +0530

    Updated Shift models with nullable fields.

--- ESPL.KP/Models/Shift/ShiftForCreationDto.cs
+++ ESPL.KP/Models/Shift/ShiftForCreationDto.cs
@@ -8,19 +8,19 @@ namespace ESPL.KP.Models
     public class ShiftForCreationDto
     {
         public string ShiftName { get; set; }
-        public TimeSpan MondayStartTime { get; set; }        
-        public TimeSpan MondayEndTime { get; set; }        
-        public TimeSpan TuesdayStartTime { get; set; }        
-        public TimeSpan TuesdayEndTime { get; set; }
-        public TimeSpan WednesdayStartTime { get; set; }        
-        public TimeSpan WednesdayEndTime { get; set; }
-        public TimeSpan ThursdayStartTime { get; set; }        
-        public TimeSpan ThursdayEndTime { get; set; }
-        public TimeSpan FridayStartTime { get; set; }        
-        public TimeSpan FridayEndTime { get; set; }
-        public TimeSpan SaturdayStartTime { get; set; }        
-        public TimeSpan SaturdayEndTime { get; set; }
-        public TimeSpan SundayStartTime { get; set; }        
-        public TimeSpan SundayEndTime { get; set; }
+        public TimeSpan? MondayStartTime { get; set; }        
+        public TimeSpan? MondayEndTime { get; set; }        
+        public TimeSpan? TuesdayStartTime { get; set; }        
+        public TimeSpan? TuesdayEndTime { get; set; }
+        public TimeSpan? WednesdayStartTime { get; set; }        
+        public TimeSpan? WednesdayEndTime { get; set; }
+        public TimeSpan? ThursdayStartTime { get; set; }        
+        public TimeSpan? ThursdayEndTime { get; set; }
+        public TimeSpan? FridayStartTime { get; set; }        
+        public TimeSpan? FridayEndTime { get; set; }
+        public TimeSpan? SaturdayStartTime { get; set; }        
+        public TimeSpan? SaturdayEndTime { get; set; }
+        public TimeSpan? SundayStartTime { get; set; }        
+        public TimeSpan? SundayEndTime { get; set; }
     }
 }
