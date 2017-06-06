4a2b04261848154444cb582952fdb091811dd278
shaggyshelar <shaggy.shelar@gmail.com>
2017-06-06 18:43:51 +0530

    Implemented generic repository

---
+++ KP.Common/Helpers/Utilities.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq.Expressions;
+
+namespace KP.Common.Helpers
+{
+    public static class Utilities
+    {
+        public static Expression<Func<TEntity, bool>> BuildLambdaForFindByKey<TEntity>(int id)
+        {
+            var item = Expression.Parameter(typeof(TEntity), "entity");
+            var prop = Expression.Property(item, typeof(TEntity).Name + "Id");
+            var value = Expression.Constant(id);
+            var equal = Expression.Equal(prop, value);
+            var lambda = Expression.Lambda<Func<TEntity, bool>>(equal, item);
+            return lambda;
+        }
+    }
+}
